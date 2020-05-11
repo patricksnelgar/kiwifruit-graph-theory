0cead41ea0bafe72fc1c3bb7554f503a9c39d221
Michael Kramer <HRTMXK@PFR.CO.NZ>
2020-05-12 10:34:17 +1200

    made changes to vine 9 csv and the .R file to fix architecture and add missing shoot information

--- code/sources/vine9.R
+++ code/sources/vine9.R
@@ -15,7 +15,7 @@ vine9_targets <- vine9_data %>%
 
 vine9_nodes <- full_join(vine9_sources, vine9_targets, by = "label") %>%
 	arrange(label) %>%
-	full_join(select(vine9_data, to, length, to_shoot_id, cane_id, to_origin_id, base_origin_id, notes), by = c("label" = "to"))
+	full_join(select(vine9_data, to, length, to_shoot_id, x, y, quadrant, cane_id, to_origin_id, base_origin_id, notes), by = c("label" = "to"))
 
 vine9_nodes %<>% 
 	mutate(target_type = ifelse(!is.na(to_shoot_id), "Shoot", ifelse(!is.na(to_origin_id), "Origin","Junction"))) %>%
@@ -24,19 +24,40 @@ vine9_nodes %<>%
 	mutate(origin_target_id = first(label)) %>%
 	ungroup()
 
+vine9_nodes %<>%
+	full_join(quadrant_info, by = c("quadrant" = "quadrant")) %>%
+	mutate(x_pos = (x + x_offset) * x_multiplier, y_pos = (y + y_offset) * y_multiplier) %>%
+	select(label:to_shoot_id, target_type, origin_target_id, x_pos, y_pos, to_origin_id) %>%
+	filter(!is.na(label))
 
 vine9_graph <- tbl_graph(vine9_nodes, vine9_data) %>%
 	activate(nodes) %>%
 	mutate(cost_to_origin = node_distance_from(origin_target_id, weights = length)) %>%
-	mutate(target_label = ifelse(!is.na(to_shoot_id), to_shoot_id, to_origin_id))
+	mutate(target_label = ifelse(!is.na(to_shoot_id), to_shoot_id, ifelse(!is.na(to_origin_id), to_origin_id, label)))
+
+ggraph(vine9_graph, layout = "manual", x = vine9_nodes$x_pos, y = vine9_nodes$y_pos) +
+	geom_edge_link(colour = "brown") +
+	geom_node_point(aes(colour = target_type), size = 5) + 
+	geom_node_text(aes(label = target_label), colour = "black", repel = TRUE) +
+	ggtitle("2D layout - Vine 9") +
+	#geom_text(x = 0, y = 1750, label = "N", size = 14) +
+	#geom_text(x = 0, y = -1700, label = "S", size = 14) +
+	#geom_vline(xintercept = c(-1000, -500, 0, 500, 1000)) +
+	#geom_hline(yintercept = c(-2000, -1000, 0, 1000, 2000)) +
+	#geom_text(aes(x = x, y = y, label = label), data = quadrant_labels, size = 14) +
+	theme_graph()
 
 
-ggraph(vine9_graph, layout = "tree") + 
+ggsave("output/graphs/kiwimac_vine9_layout.png", width = 20, height = 20)
+
+
+ggraph(vine9_graph, "tree") +
 	geom_edge_link(colour = "brown") +
 	geom_node_point(aes(colour = target_type), size = 6) +
-	geom_node_text(aes(label = target_label, colour = target_type), repel = TRUE, size = 6) +
-	ggtitle("Kiwimac - Vine 9 architecture") + 
+	geom_node_text(aes(label = target_label, colour = target_type), repel = TRUE) +
+	ggtitle("Kiwimac - Vine 9 architecture") +
 	theme_graph() +
 	theme(text = element_text(size = 14), title = element_text(size = 18))
 
-ggsave("output/graphs/kiwimac_vine9.png", width = 49, height = 20)
+ggsave("output/graphs/kiwimac_vine9.png", width = 35, height = 20)
+
