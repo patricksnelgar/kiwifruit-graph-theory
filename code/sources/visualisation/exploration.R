e0b4486341266bb29971751e4abbd22729e06a4f
Patrick Snelgar <patricksnelgar@users.noreply.github.com>
2020-06-02 09:22:54 +1200

    sync commit from laptop. started exploring qaudrant visualisation.

--- code/sources/visualisation/exploration.R
+++ code/sources/visualisation/exploration.R
@@ -1,11 +1,15 @@
 
 
-grid_quadrants <- data.frame(row = rep(6:1, each = 6), column = rep(1:6, 6), quadrant = 1:36)
 
+grid_quadrants <- data.frame(row = rep(1:6, each = 6), column = rep(1:6, 6), quadrant = 1:36)
 
-summarised_fruit _data <- all_fruit_data %>%
+summarised_fruit_data <- all_fruit_data %>%
+	filter(!is.na(DryMatter) & !is.na(FreshWeight)) %>%
 	group_by(Vine, quadrant) %>%
-	summarise(avg_dm = mean(DryMatter, na.rm = TRUE), avg_fw = mean(FreshWeight, na.rm = TRUE))
+	summarise(avg_dm = mean(DryMatter, na.rm = TRUE), avg_fw = mean(FreshWeight, na.rm = TRUE)) %>%
+	mutate(dm_bins = cut(avg_dm, c(0,14:23,30), include.lowest = TRUE, labels = c("< 14", paste(14:22, "to", 15:23), "> 23"))) %>%
+	left_join(grid_quadrants, by = c("quadrant" = "quadrant")) %>%
+	left_join(relative_quadrants, by = c("quadrant" = "quadrant"))
 
 summarised_fruit_data %>%
 	ggplot() +
@@ -21,5 +25,15 @@ summarised_fruit_data %>%
 summarised_fruit_data %>%
 	left_join(grid_quadrants, by = c("quadrant" = "quadrant")) %>%
 	ggplot() +
-	geom_bin2d(aes(column, row, fill = avg_dm), stat = "identity") +
-	facet_wrap(~Vine)
+	geom_bin2d(aes(column, row, fill = dm_bins), stat = "identity") +
+	scale_fill_brewer(name = "Dry matter (%)", type = "div", palette = "RdYlBu", direction = "-1") +
+	facet_wrap(~Vine) + 
+	ggtitle("Average Dry matter by quadrant, by vine") +
+	theme_bw()
+
+
+summarised_fruit_data %>%
+	ggplot() +
+		geom_point(aes(QuadrantFromLeader, avg_dm)) +
+		theme_bw()
+
